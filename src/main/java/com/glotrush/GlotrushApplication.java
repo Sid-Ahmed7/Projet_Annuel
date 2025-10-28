89890433d5001810a7bc8ae022f3edd23ea18e79
Sid-Ahmed7 <sido91080@hotmail.fr>
2025-10-29 00:34:13 +0100

    first commit

---
+++ src/main/java/com/glotrush/GlotrushApplication.java
@@ -0,0 +1,13 @@
+package com.glotrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlotrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlotrushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glotrush/GlotrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glotrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlotrushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
