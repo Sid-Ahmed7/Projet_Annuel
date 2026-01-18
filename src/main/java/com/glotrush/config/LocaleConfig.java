a957ce5a004a9ed356385af78fa110dd4a0e4880
LuzBoger <arthurbrouard2003@gmail.com>
2026-01-18 11:20:18 +0100

    ajout de la localConfig

--- src/main/java/com/glotrush/config/LocaleConfig.java
+++ src/main/java/com/glotrush/config/LocaleConfig.java
@@ -1,4 +1,37 @@
 package com.glotrush.config;
 
-public class LocaleConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver("myAppLocaleCookie"); // Nom du cookie passé au constructeur
+        resolver.setDefaultLocale(Locale.FRENCH);
+        resolver.setCookieMaxAge(Duration.ofDays(30)); // 30 jours
+        resolver.setCookiePath("/");
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
 }
