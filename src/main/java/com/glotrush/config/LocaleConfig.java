4d263f729718d5410936ff3aa1673ef438e5aa10
LuzBoger <arthurbrouard2003@gmail.com>
2026-01-27 16:14:49 +0100

    fix locale config and application.properties

--- src/main/java/com/glotrush/config/LocaleConfig.java
+++ src/main/java/com/glotrush/config/LocaleConfig.java
@@ -1,18 +1,29 @@
 package com.glotrush.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Locale;
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return messageSource;
+    }
+
     @Bean
     public LocaleResolver localeResolver() {
         CookieLocaleResolver resolver = new CookieLocaleResolver("myAppLocaleCookie"); // Nom du cookie passé au constructeur
