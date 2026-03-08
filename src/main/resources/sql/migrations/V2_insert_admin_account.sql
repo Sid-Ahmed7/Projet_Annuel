7b4b5ae5f3835c5a821be5cc4fda5a2a88f40f1e
Sid-Ahmed7 <sido91080@hotmail.fr>
2026-03-08 16:57:08 +0100

    change variables

--- src/main/resources/sql/migrations/V2_insert_admin_account.sql
+++ src/main/resources/sql/migrations/V2_insert_admin_account.sql
@@ -14,14 +14,14 @@ INSERT INTO accounts (
     updated_at
 ) VALUES (
     gen_random_uuid(),
-    '${admin_email}',                    
-    '${admin_password}',           
-    '${admin_first_name}',               
-    '${admin_last_name}',                
+    '${email}',                    
+    '${password}',           
+    '${first_name}',               
+    '${last_name}',                
     'admin',                             
     'ADMIN',                             
     'ACTIVE',                          
-    '${admin_secret_key}',          
+    '${secret_key}',          
     0,                                  
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP
