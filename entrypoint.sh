5264e469416747944e466576448ae9f2a589e7af
LuzBoger <arthurbrouard2003@gmail.com>
2026-04-05 13:43:23 +0200

    modification securityConfig, entrypoint.sh and application.properties for deployement in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,6 @@ export STRIPE_WEBHOOK_SECRET=$(cat /run/secrets/stripe_webhook_secret)
 export FLYWAY_ADMIN_PASS=$(cat /run/secrets/flyway_admin_pass)
 export FLYWAY_SECRET_KEY=$(cat /run/secrets/flyway_secret_key)
 export FLYWAY_EMAIL=$(cat /run/secrets/flyway_email)
-
+export FLYWAY_LAST_NAME=$(cat /run/secrets/flyway_last_name)
+export FLYWAY_FIRST_NAME=$(cat /run/secrets/flyway_first_name)
 exec java -jar /app/app.jar
