1348dcf3e4e54f410ad687e2856adf1595d824fb
LuzBoger <arthurbrouard2003@gmail.com>
2026-06-03 15:40:13 +0200

    ajout du système permettant d'avoir des système d'apprentissage via ai

--- src/main/java/com/glotrush/dto/request/exercice/MatchingPairRequest.java
+++ src/main/java/com/glotrush/dto/request/exercice/MatchingPairRequest.java
@@ -4,9 +4,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.UUID;
 
 @Data
 public class MatchingPairRequest {
+    private UUID id;
+
     @NotNull
     @NotEmpty
     private String item1;
