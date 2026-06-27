af1daa2182ca267a3e11109c8c0a961479376f9e
Sid-Ahmed7 <sido91080@hotmail.fr>
2026-06-27 19:01:22 +0200

    fix pattern for mdp

--- src/main/java/com/glotrush/dto/request/AdminResetPasswordRequest.java
+++ src/main/java/com/glotrush/dto/request/AdminResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.glotrush.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,9 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminResetPasswordRequest {
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "{error.password.required}")
+    @Size(min = 12, message = "{error.password.minLength}")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{12,}$",
+            message = "{error.password.strong}")
     private String newPassword;
 }
